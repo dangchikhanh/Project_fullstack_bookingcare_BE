66da46ed49c42ea6d5100d9243af6112e8322226
dangchikhanh <61544957+dangchikhanh@users.noreply.github.com>
2023-06-22 16:15:22 +0700

    25:30 at #27

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -3,14 +3,17 @@ import CRUDService from "../services/CRUDService";
 let getHomePage = (req, res) => {
     return res.render('homePage.ejs');
 }
+
 let getCRUD = (req, res) => {
     return res.render('crud.ejs');
 }
+
 let postCRUD = async(req, res) => {
     let message = await CRUDService.createNewUser(req.body);
     console.log(message);
     return res.send('Port CRUD from server');
-};
+}
+
 let displayGetCRUD = async(req, res) => {
     let data = await CRUDService.getAllUser();
     console.log(data);
@@ -18,9 +21,22 @@ let displayGetCRUD = async(req, res) => {
         dataTable: data
     });
 }
+
+let getEditCRUD = async(req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoId(userId);
+        //Check if user data not found
+        return res.send('User found: ' + JSON.stringify(userData));
+    } else {
+        return res.send('User not found');
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
 }
