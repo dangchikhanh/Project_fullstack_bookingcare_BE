b6cac0b748ea452610eb2f6f4489585e0f4495b9
dangchikhanh <61544957+dangchikhanh@users.noreply.github.com>
2023-06-22 14:43:29 +0700

    #25 done

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,18 @@
+import CRUDService from "../services/CRUDService";
+
 let getHomePage = (req, res) => {
     return res.render('homePage.ejs');
 }
-
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async(req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('Port CRUD from server');
+};
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
